12d0553767621aefb1fd4d6afb7b1c1ebfc14117
Charis Fernandes <168012873+Charis-Fernandes@users.noreply.github.com>
2026-06-25 20:04:29 +0530

    Update firebase.config.ts

--- config/firebase.config.ts
+++ config/firebase.config.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBum1wsHXVSV-7vH34Ph3rGloz8acu6hYs",
-  authDomain: "lookback-30b40.firebaseapp.com",
-  projectId: "lookback-30b40",
-  storageBucket: "lookback-30b40.firebasestorage.app",
-  messagingSenderId: "939967695465",
-  appId: "1:939967695465:web:f4feb93abb23c6974f30f8",
-  measurementId: "G-JNS0V0Y0Z6"
+  apiKey: "",
+  authDomain: "l,
+  projectId: "l",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ",
+  measurementId: ""
 };
 
 // Initialize Firebase
